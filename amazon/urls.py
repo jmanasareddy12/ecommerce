90b1784e938c41a0b630b1d2744822f481fbdd20
jmanasareddy <jmanasareddy12@gmail.com>
2026-02-16 14:47:56 +0530

    further modification of cart

--- amazon/urls.py
+++ amazon/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
-    path('',views.product_list,name='product_list'),
-    path('product/<int:id>/',views.product_details,name='product_details'),
-    path('add/<int:id>/',views.add_to_cart,name='add_to_cart'),
-    path('cart/<int:id>/',views.view_cart,name='view_cart'),
+urlpatterns = [
+    path('', views.product_list, name='product_list'),
+    path('product/<int:id>/', views.product_details, name='product_details'),
+    path('add/<int:id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.view_cart, name='view_cart'),
+    path('cart/increase/<int:id>/', views.increase, name='increase'),
+    path('cart/decrease/<int:id>/', views.decrease, name='decrease'),
+    path('cart/remove/<int:id>/', views.remove, name='remove'),
 ]
--- amazon/views.py
+++ amazon/views.py
@@ -1,22 +1,76 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product
-# Create your views here.
 
 def product_list(request):
-    products=Product.objects.all()
-    return render (request,'store/product_list.html',{'products':products})
+    products = Product.objects.all()
+    return render(request, 'store/product_list.html', {'products': products})
 
-def product_details(request,id):
-    product = Product.objects.get(id=id)
-    return render(request,'store/product_details.html',{'product':product})
+def product_details(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'store/product_details.html', {'product': product})
 
-def add_to_cart(request,id):
-    cart=request.session.get('cart',{})
-    cart[id]=cart.get(id,0)+1
-    request.session['cart']=cart
-    return redirect('view_cart',id=id)
+def add_to_cart(request, id):
+    cart = request.session.get('cart', {})
+
+    id = str(id)  # store keys as strings
+    if id in cart:
+        cart[id] += 1
+    else:
+        cart[id] = 1
+
+    request.session['cart'] = cart
+    return redirect('view_cart')
 
 def view_cart(request):
-    cart=request.session.get('cart',{})
-    products= Product.objects.filter(id__in=cart.keys())
-    return render(request,'store/cart.html',{'products':products,'cart':cart})
+    cart = request.session.get('cart', {})
+    cart_items = []
+    final_amount = 0
+
+    for id, quantity in cart.items():
+        product = get_object_or_404(Product, id=int(id))
+        total = product.price * quantity
+        final_amount += total
+
+        cart_items.append({
+            'product': product,
+            'quantity': quantity,
+            'total': total
+        })
+
+    return render(request, 'store/cart.html', {
+        'cart_items': cart_items,
+        'final_amount': final_amount
+    })
+
+def increase(request, id):
+    cart = request.session.get('cart', {})
+    id = str(id)
+
+    if id in cart:
+        cart[id] += 1
+
+    request.session['cart'] = cart
+    return redirect('view_cart')
+
+def decrease(request, id):
+    cart = request.session.get('cart', {})
+    id = str(id)
+
+    if id in cart:
+        if cart[id] > 1:
+            cart[id] -= 1
+        else:
+            del cart[id]
+
+    request.session['cart'] = cart
+    return redirect('view_cart')
+
+def remove(request, id):
+    cart = request.session.get('cart', {})
+    id = str(id)
+
+    if id in cart:
+        del cart[id]
+
+    request.session['cart'] = cart
+    return redirect('view_cart')
