d585f15c33ba3660345ffed275a5279d4fe08ed5
jmanasareddy <jmanasareddy12@gmail.com>
2026-02-16 09:46:36 +0530

    setting up models

--- amazon/models.py
+++ amazon/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Product(models.Model):
     name=models.CharField(max_length=50)
     price=models.DecimalField(max_digits=8,decimal_places=2)
+    description=models.TextField()
+    image=models.ImageField(upload_to='products/')
+    
