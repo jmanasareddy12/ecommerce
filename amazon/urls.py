9e9c9196f0742d14508c7621cff3004d644b330e
jmanasareddy <jmanasareddy12@gmail.com>
2026-02-16 11:18:31 +0530

    adding more html

--- amazon/urls.py
+++ amazon/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns=[
     path('',views.product_list,name='product_list'),
-    
+    path('product/<int:id>/',views.product_details,name='product_details'),
 ]
--- amazon/views.py
+++ amazon/views.py
@@ -5,3 +5,7 @@ from .models import Product
 def product_list(request):
     products=Product.objects.all()
     return render (request,'store/product_list.html',{'products':products})
+
+def product_details(request,id):
+    product = Product.objects.get(id=id)
+    return render(request,'store/product_details.html',{'product':product})
