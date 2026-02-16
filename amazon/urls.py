84df6e492e73a7a3f22a48ea10ae90d210ba873f
jmanasareddy <jmanasareddy12@gmail.com>
2026-02-16 12:17:37 +0530

    added cart

--- amazon/urls.py
+++ amazon/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns=[
     path('',views.product_list,name='product_list'),
     path('product/<int:id>/',views.product_details,name='product_details'),
+    path('add/<int:id>/',views.add_to_cart,name='add_to_cart'),
+    path('cart/<int:id>/',views.view_cart,name='view_cart'),
 ]
--- amazon/views.py
+++ amazon/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Product
 # Create your views here.
 
@@ -9,3 +9,14 @@ def product_list(request):
 def product_details(request,id):
     product = Product.objects.get(id=id)
     return render(request,'store/product_details.html',{'product':product})
+
+def add_to_cart(request,id):
+    cart=Product.session.get('cart',{})
+    cart[id]=cart.get(id,0)+1
+    request.session['cart']=cart
+    return redirect('product_list')
+
+def view_cart(request):
+    cart=request.session.get('cart',{})
+    products= Product.objects.filter(id__in=cart.keys())
+    return render(request,'store/cart.html',{'products':products,'cart':cart})
